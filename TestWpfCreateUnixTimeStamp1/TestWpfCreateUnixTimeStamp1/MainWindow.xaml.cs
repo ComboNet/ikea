45d251dd6c91be15680831e0870f58583fc84a22
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 09:02:29 +0700

    TestWpfCreateUnixTimeStamp1

---
+++ TestWpfCreateUnixTimeStamp1/TestWpfCreateUnixTimeStamp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace TestWpfCreateUnixTimeStamp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Int32 unixTimestamp = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            // DateTime now = DateTime.UtcNow;
+            listBox1.Items.Add(unixTimestamp.ToString());
+        }
+    }
+}
