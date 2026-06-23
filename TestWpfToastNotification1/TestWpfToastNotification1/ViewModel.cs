1206f1d87a738c220d840c2c63c8c16d40881710
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:45:15 +0700

    TestWpfToastNotification1

---
+++ TestWpfToastNotification1/TestWpfToastNotification1/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Notification.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWpfToastNotification1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        public ViewModel()
+        {
+            
+        }
+
+        [ObservableProperty] public ObservableCollection<string> _items = new();
+
+        public ICommand OkCommand => new RelayCommand(() =>
+        {
+            var notificationManager = new NotificationManager();
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Sample notification",
+                Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                Type = NotificationType.Information,
+            }, expirationTime: TimeSpan.FromSeconds(0.5));
+        });
+    }
+}
