4a8e252d8c432e062def511b597d524162f9c551
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:55:14 +0700

    TestWpfDependencyInjection1

---
+++ TestWpfDependencyInjection1/TestWpfDependencyInjection1/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using TestWpfDependencyInjection1.Data;
+
+namespace TestWpfDependencyInjection1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddDbContext<EmployeeDbContext>(options =>
+            {
+                options.UseSqlite("Data Source = Employee.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+
+}
---
+++ TestWpfDependencyInjection1/TestWpfDependencyInjection1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using TestWpfDependencyInjection1.Data;
+
+namespace TestWpfDependencyInjection1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmployeeDbContext dbContext;
+
+        public MainWindow(EmployeeDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            InitializeComponent();
+            GetEmployees();
+        }
+
+        private void GetEmployees()
+        {
+            var employees = dbContext.Employee.ToList();
+            EmployeeDG.ItemsSource = employees;
+        }
+    }
+}
