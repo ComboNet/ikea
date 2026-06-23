2bd23a99fcbcb20e3c79eb8976ba8a4b967c0c35
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 09:26:42 +0700

    TestWpfRotateAnimation1

---
+++ TestWpfRotateAnimation1/TestWpfRotateAnimation1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWpfRotateAnimation1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        [ObservableProperty] public bool _isRotate = false;
+
+        public ICommand OkCommand => new RelayCommand(() =>
+        {
+                IsRotate = !IsRotate;
+        });
+    }
+}
