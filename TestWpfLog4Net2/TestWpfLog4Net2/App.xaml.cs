0ab5d475c12cc28d211bca3383cb891bf5c5fecf
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:56:01 +0700

    TestWpfLog4Net2

---
+++ TestWpfLog4Net2/TestWpfLog4Net2/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using System.Windows;
+
+namespace TestWpfLog4Net2
+{
+    public partial class App : Application
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            log.Info("        =============  Started Logging  =============        ");
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ TestWpfLog4Net2/TestWpfLog4Net2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using log4net;
+
+namespace TestWpfLog4Net2
+{
+    public partial class MainWindow : Window
+    {
+        FileSystemWatcher watcher;
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        string fileName = @"C:\log\log.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            log4net.Config.XmlConfigurator.Configure();
+            ChangeLogLocation(fileName);
+        }
+        private void ChangeLogLocation(string filePath)
+        {
+            var rootRepository = log4net.LogManager.GetRepository();
+            foreach (var appender in rootRepository.GetAppenders())
+            {
+                if (appender.Name.Equals("file") && appender is log4net.Appender.FileAppender)
+                {
+                    var fileAppender = appender as log4net.Appender.FileAppender;
+                    fileAppender.File = filePath;
+                    fileAppender.ActivateOptions();
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            log.Debug("Hello World");
+        }
+    }
+}
