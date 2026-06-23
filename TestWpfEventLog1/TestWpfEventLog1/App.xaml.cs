bf5921e5113809eb14b2b681a7c50f1faaaaba1c
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:54:26 +0700

    TestWpfEventLog1

---
+++ TestWpfEventLog1/TestWpfEventLog1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+using System.Windows;
+
+namespace TestWpfEventLog1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+
+}
---
+++ TestWpfEventLog1/TestWpfEventLog1/ViewModel.cs
@@ -0,0 +1,75 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Windows.Input;
+
+namespace TestWpfEventLog1;
+
+// Need run as Administrator
+
+[ObservableObject]
+public partial class ViewModel
+{
+    #region Fields
+    string sourceName = "SampleApplicationSource";
+    string myLogName = "myNewLog";
+    string messageFile = "customlog.log";
+    EventLog eventLog;
+    #endregion
+
+    [ObservableProperty] public ObservableCollection<string> _items = new();
+
+    public ViewModel()
+    {
+        if (!IsRunningAsAdministrator())
+        {
+            string fileExe = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(fileExe);
+            processStartInfo.UseShellExecute = true;
+            processStartInfo.Verb = "runas";
+            Process.Start(processStartInfo);
+            App.Current.Shutdown();
+        }
+        eventLog = new EventLog(myLogName, ".", sourceName);
+        eventLog.Source = sourceName;
+    }
+    public bool IsRunningAsAdministrator()
+    {
+        WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent();
+        WindowsPrincipal windowsPrincipal = new WindowsPrincipal(windowsIdentity);
+        return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+    private void WriteToApplicationEventLog(string logMessage, EventLogEntryType logType, int eventID)
+    {
+        if (!EventLog.SourceExists(sourceName, Environment.MachineName))
+        {
+            EventSourceCreationData sourceData = new EventSourceCreationData(sourceName, myLogName);
+            sourceData.MessageResourceFile = sourceData.CategoryResourceFile = sourceData.ParameterResourceFile = messageFile;
+            sourceData.CategoryCount = 1;
+            EventLog.CreateEventSource(sourceData);
+        }
+        myLogName = EventLog.LogNameFromSourceName(sourceName, ".");
+        eventLog.WriteEntry(logMessage, logType, eventID);
+    }
+    public ICommand AddLogCommand => new RelayCommand(() =>
+    {
+        WriteToApplicationEventLog("test", EventLogEntryType.Error, 123);
+        WriteToApplicationEventLog("test", EventLogEntryType.Warning, 123);
+        WriteToApplicationEventLog("test", EventLogEntryType.Information, 123);
+    });
+    public ICommand ClearLogsCommand => new RelayCommand(() => { eventLog.Clear(); });
+    public ICommand DeleteEventLogCommand => new RelayCommand(() => { EventLog.Delete(myLogName, Environment.MachineName); });
+    public ICommand ReadLogsCommand => new RelayCommand(() =>
+    {
+        Items.Clear();
+        EventLog log = new EventLog("myNewLog");
+        foreach (EventLogEntry entry in eventLog.Entries)
+        {
+            string msg = entry.Message.Trim();
+            string data = msg.Substring(msg.IndexOf(':') + 2, msg.Length - msg.IndexOf(':') - 3);
+            Items.Add(entry.TimeGenerated +","+ entry.EntryType + "," + data + "," + entry.EventID.ToString());
+        }
+    });
+}
