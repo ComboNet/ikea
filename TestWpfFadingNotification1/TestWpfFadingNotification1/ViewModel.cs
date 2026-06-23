d6c71e492723a6de80bf2652d0c7e02293ac1493
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:49:29 +0700

    TestWpfFadingNotification1

---
+++ TestWpfFadingNotification1/TestWpfFadingNotification1/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWpfFadingNotification1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        [ObservableProperty] public bool _isVisible = false;
+        public ICommand TrueCommand => new RelayCommand(() => { IsVisible = true; });
+        public ICommand FalseCommand => new RelayCommand(() => { IsVisible = false; });
+    }
+}
