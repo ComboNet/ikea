30a8e199ff4b8e0250a4e47e2946d1625a829d25
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:54:49 +0700

    DemoEventViewer

---
+++ DemoEventViewer/DemoEventViewer/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+string logName = "DemoApp";
+string sourceName = "SourceDemo";
+
+if(!EventLog.SourceExists(sourceName, Environment.MachineName))
+{
+    EventLog.CreateEventSource(new EventSourceCreationData(sourceName, logName));
+}
