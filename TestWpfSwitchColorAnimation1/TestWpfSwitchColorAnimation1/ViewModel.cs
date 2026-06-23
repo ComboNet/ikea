d4681624a79eaf5213a9958c7c7aa874070a713d
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:52:19 +0700

    TestWpfSwitchColorAnimation1

---
+++ TestWpfSwitchColorAnimation1/TestWpfSwitchColorAnimation1/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWpfSwitchColorAnimation1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        #region Properties
+        [ObservableProperty] public ObservableCollection<string> _items = new();
+        [ObservableProperty] public bool _isTrigger = false;
+        #endregion
+
+        public ICommand OkCommand => new RelayCommand(() =>
+        {
+            IsTrigger = true;   
+        });
+    }
+}
