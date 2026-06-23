52334a3fde073221abe64c5d4ff3abf6cbf552c1
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:53:58 +0700

    TestWpfBlinking1

---
+++ TestWpfBlinking1/TestWpfBlinking1/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace TestWpfBlinking1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        [ObservableProperty] public bool _isEnabled = false;
+
+        public ICommand EnableCommand => new RelayCommand(() => { IsEnabled = true; });
+        public ICommand DisableCommand => new RelayCommand(() => { IsEnabled = false; });
+    }
+}
