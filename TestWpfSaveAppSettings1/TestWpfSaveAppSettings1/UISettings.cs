ff8957aa093752f39f459ef89428961eb4331859
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:56:26 +0700

    TestWpfSaveAppSettings1

---
+++ TestWpfSaveAppSettings1/TestWpfSaveAppSettings1/UISettings.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Configuration;
+
+namespace TestWpfSaveAppSettings1
+{
+    [ObservableObject]
+    public partial class UISettings:ConfigurationSection
+    {
+        /*[ObservableProperty] public string _language= "English";
+        [ObservableProperty] public string _theme = "Light";*/
+
+
+        [ConfigurationProperty("language", DefaultValue = "English")]
+        public string Language
+        {
+            get { return (string)this["language"]; }
+            set { this["language"] = value; }
+        }
+
+        [ConfigurationProperty("theme", DefaultValue = "Light")]
+        public string Theme
+        {
+            get { return (string)this["theme"]; }
+            set { this["theme"] = value; }
+        }
+    }
+}
---
+++ TestWpfSaveAppSettings1/TestWpfSaveAppSettings1/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TestWpfSaveAppSettings1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        #region Fields
+        Configuration AppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        UISettings uiSetting; 
+        #endregion
+
+        #region Properties
+        [ObservableProperty] public ObservableCollection<string> _languages = new() { "English", "Chinese" };
+        [ObservableProperty] public ObservableCollection<string> _themes = new() { "Dark", "Light" };
+        [ObservableProperty] public string _selectedLanguage = String.Empty;
+        [ObservableProperty] public string _selectedTheme = String.Empty;
+        #endregion
+
+        public ViewModel()
+        {
+            uiSetting = new UISettings();
+            if (AppConfig.Sections["UISettings"] is null)
+                AppConfig.Sections.Add("UISettings", new UISettings());
+            var uiSettings = (UISettings)AppConfig.GetSection("UISettings");
+            SelectedLanguage = uiSettings.Language;
+            SelectedTheme = uiSettings.Theme;
+        }
+
+        public ICommand SaveCommand => new RelayCommand(() =>
+        {
+            if (AppConfig.Sections["UISettings"] is not null)
+                AppConfig.Sections.Remove("UISettings");
+            uiSetting.Language = SelectedLanguage;
+            uiSetting.Theme = SelectedTheme;
+            AppConfig.Sections.Add("UISettings", uiSetting);
+            AppConfig.Save();
+        });
+    }
+}
