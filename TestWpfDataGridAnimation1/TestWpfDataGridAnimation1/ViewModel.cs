52ee3d99c8edfd0c7ff44c1cb77a72d154f3369c
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 09:03:18 +0700

    TestWpfDataGridAnimation1

---
+++ TestWpfDataGridAnimation1/TestWpfDataGridAnimation1/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace TestWpfDataGridAnimation1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        public ViewModel()
+        {
+            Items = new ObservableCollection<Data>()
+            {
+                new Data() { Id = 1, Name = "Hello" },
+                new Data() { Id = 2, Name = "World" },
+                new Data() { Id = 3, Name = "Narongsak" }
+            };
+        }
+        [ObservableProperty] public ObservableCollection<Data> _items = new();
+        public ICommand OkCommand => new RelayCommand(() =>
+        {
+
+        });
+    }
+
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
