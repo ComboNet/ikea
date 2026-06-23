73c1479ffa1c74976da748abdb76c546fcf0384f
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 09:01:07 +0700

    TestWpfDecimalUpDown1

---
+++ TestWpfDecimalUpDown1/TestWpfDecimalUpDown1/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+
+namespace TestWpfDecimalUpDown1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ TestWpfDecimalUpDown1/TestWpfDecimalUpDown1/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MahApps.Metro.Controls;
+
+namespace TestWpfDecimalUpDown1
+{
+    [ObservableObject]
+    public partial class ViewModel
+    {
+        [ObservableProperty] public double value = 34.1;
+        [ObservableProperty] public NumericInput _numericInput = NumericInput.Numbers;
+        [ObservableProperty] public double _interval = 1;
+    }
+}
+
+
