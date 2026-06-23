d88ef25d6f796dc88c076d96eda456db0eef95e2
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2026-06-23 08:39:51 +0700

    TestWpfTransitioner1

---
+++ TestWpfTransitioner1/TestWpfTransitioner1/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TestWpfTransitioner1
+{ 
+    [ObservableObject]
+    public partial class ViewModel
+    {
+
+    }
+}
